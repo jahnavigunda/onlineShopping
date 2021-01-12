2b7ef0348ddcf7d46d57e0370a58de22af5cd3cd
jahnavigunda <jahnavi.gunda93@gmail.com>
2021-01-11 17:49:05 -0800

    CRUD endpoints for controller and integrated mongoDB Added Lombok and data validation

--- src/main/java/com/onlineshopping/product/controller/ProductController.java
+++ src/main/java/com/onlineshopping/product/controller/ProductController.java
@@ -2,18 +2,18 @@ package com.onlineshopping.product.controller;
 
 import com.onlineshopping.product.dto.Product;
 import com.onlineshopping.product.service.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class ProductController {
-    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     private ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -21,9 +21,9 @@ public class ProductController {
     }
 
     @PostMapping("/addProduct")
-    ResponseEntity<Product> addProduct(@RequestBody Product product) {
+    ResponseEntity<Product> addProduct(@RequestBody @Valid Product product) {
         String status = productService.addProduct(product);
-        logger.info("Product added status - {}", status);
+        log.info("Product added status - {}", status);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
--- src/main/java/com/onlineshopping/product/dto/Product.java
+++ src/main/java/com/onlineshopping/product/dto/Product.java
@@ -1,87 +1,32 @@
 package com.onlineshopping.product.dto;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Document(collection = "product")
 public class Product {
     @Id
     private Integer id;
+    @NotNull(message = "Product Name should not be null")
     private String name;
+    @NotNull(message = "Category should not be null")
     private Category category;
+    @Min(0)
     private double price;
     private String currency;
+    @Max(100)
     private double discount;
     private String discountDescription;
     private List<String> imageURLs;
-
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public List<String> getImageURLs() {
-        return imageURLs;
-    }
-
-    public void setImageURLs(List<String> imageURLs) {
-        this.imageURLs = imageURLs;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
-
-    public String getDiscountDescription() {
-        return discountDescription;
-    }
-
-    public void setDiscountDescription(String discountDescription) {
-        this.discountDescription = discountDescription;
-    }
 
 
 }
